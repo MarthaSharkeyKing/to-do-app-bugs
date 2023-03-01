299ccfeddd15061ff7f98225c9ce2aa3a162e212
Martha King (She/Her) <mking@scottlogic.com>
2023-03-01 13:20:00 +0000

    add

--- src/App.js
+++ src/App.js
@@ -1,56 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React, {useState} from 'react';
+
 import './App.css';
 
 function App() {
+
+  const [currentToDoText, setText] = useState(''); 
+  const [todos, setTodos] = useState([]);
+  const [name, setName] = useState('');
+
+  const handleInputChange = (event) => {
+    setText(event.target.value)
+  }
+
+  const handleAdd = () => {
+    setTodos([...todos, {id: todos.length, title: currentToDoText}])
+  }
+
+  const handleNameChange = (event) => {
+    setName(`${name} ${event.target.value}`)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+      <header>
+        TO DO APP: 
       </header>
+
+      <input type="text" placeholder='Enter your name' onChange={handleNameChange}></input>
+      <div>Hello {name}</div>
+
+      <input type="text" onChange={handleInputChange}></input>
+      <button onClick={handleAdd}>Add</button>
+
+      <div>Number of to dos: {todos.length}</div>
+
+      <div className='List'>
+        To Do List: 
+        {todos.filter(todo => todo.id).map(todo => {
+          return( 
+            <div className='ListItem'>{todo.title}</div>
+          )
+        })}
+      </div>
     </div>
   );
 }
